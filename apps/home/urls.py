79fd6c0386b161682cec42881db8a45c7ba228ff
Lee Khoon Hong <69204505+KhoonHong@users.noreply.github.com>
2023-09-30 21:24:10 +0800

    camera

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -8,14 +8,12 @@ from apps.home import views
 from .views import camera_feed, live_camera_feed
 
 urlpatterns = [
+    path('camera/', camera_feed, name='camera_feed'),
+    path('live_camera_feed/', live_camera_feed, name='live_camera_feed'),
 
     # The home page
     path('', views.index, name='home'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
-
-    path('camera/', camera_feed, name='camera_feed'),
-    path('live_camera_feed/', live_camera_feed, name='live_camera_feed')
-
 ]
