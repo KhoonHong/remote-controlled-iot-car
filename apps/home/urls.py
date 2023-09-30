84c067ccb88d7f0456a449e7d6a368fe94116b60
Lee Khoon Hong <69204505+KhoonHong@users.noreply.github.com>
2023-09-30 21:17:29 +0800

    camera

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -5,6 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from apps.home import views
+from .views import login_view, register_user, camera_feed, live_camera_feed
 
 urlpatterns = [
 
@@ -13,5 +14,8 @@ urlpatterns = [
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
+
+    path('camera/', camera_feed, name='camera_feed'),
+    path('live_camera_feed/', live_camera_feed, name='live_camera_feed')
 
 ]
