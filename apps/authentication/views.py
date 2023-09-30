947d66e697add5aad5dd15a15fc9952254cd8b65
Lee Khoon Hong <69204505+KhoonHong@users.noreply.github.com>
2023-09-30 20:30:16 +0800

    camera

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -58,3 +58,28 @@ def register_user(request):
 
 def live_camera_feed(request):
     return render(request, 'accounts/live_camera_feed.html')
+
+
+import io
+import picamera
+from django.http import StreamingHttpResponse
+
+class Camera:
+    def stream(self):
+        with picamera.PiCamera() as camera:
+            camera.resolution = (640, 480)
+            camera.framerate = 24
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(stream, format='jpeg', quality=70, use_video_port=True):
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + stream.getvalue() + b'\r\n')
+                stream.seek(0)
+                stream.truncate()
+
+camera = Camera()
+
+def gen(camera):
+    return camera.stream()
+
+def camera_feed(request):
+    return StreamingHttpResponse(gen(camera), content_type='multipart/x-mixed-replace; boundary=frame')
