1b2197ada6b6bf9b4b81b6065e39f5d2cdeb07fd
Lee Khoon Hong <69204505+KhoonHong@users.noreply.github.com>
2023-09-30 20:52:45 +0800

    camera

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -5,6 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from apps.home import views
+from django.conf.urls import include
 
 urlpatterns = [
 
@@ -13,5 +14,7 @@ urlpatterns = [
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
+
+    path('', include('home.urls'))
 
 ]
