41a99e570745dc6340b9735299fde1e449f1d408
Lee Khoon Hong <69204505+KhoonHong@users.noreply.github.com>
2023-09-30 21:08:36 +0800

    camera

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -8,9 +8,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, SignUpForm
 
-import io
-import picamera
-from django.http import StreamingHttpResponse
 
 
 def login_view(request):
@@ -60,26 +57,3 @@ def register_user(request):
     return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
 
 
-def live_camera_feed(request):
-    return render(request, 'accounts/test.html')
-
-
-class Camera:
-    def stream(self):
-        with picamera.PiCamera() as camera:
-            camera.resolution = (640, 480)
-            camera.framerate = 24
-            stream = io.BytesIO()
-            for _ in camera.capture_continuous(stream, format='jpeg', quality=70, use_video_port=True):
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + stream.getvalue() + b'\r\n')
-                stream.seek(0)
-                stream.truncate()
-
-camera = Camera()
-
-def gen(camera):
-    return camera.stream()
-
-def camera_feed(request):
-    return StreamingHttpResponse(gen(camera), content_type='multipart/x-mixed-replace; boundary=frame')
--- apps/home/views.py
+++ apps/home/views.py
@@ -8,6 +8,12 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.shortcuts import render, redirect
+
+
+import io
+import picamera
+from django.http import StreamingHttpResponse
 
 
 @login_required(login_url="/login/")
@@ -42,3 +48,28 @@ def pages(request):
     except:
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
+
+
+def live_camera_feed(request):
+    return render(request, 'accounts/test.html')
+
+
+class Camera:
+    def stream(self):
+        with picamera.PiCamera() as camera:
+            camera.resolution = (640, 480)
+            camera.framerate = 24
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(stream, format='jpeg', quality=70, use_video_port=True):
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + stream.getvalue() + b'\r\n')
+                stream.seek(0)
+                stream.truncate()
+
+camera = Camera()
+
+def gen(camera):
+    return camera.stream()
+
+def camera_feed(request):
+    return StreamingHttpResponse(gen(camera), content_type='multipart/x-mixed-replace; boundary=frame')
