c81a4a247511ef9f9567b1d474d80b16be2db556
Lee Khoon Hong <69204505+KhoonHong@users.noreply.github.com>
2023-10-02 19:47:24 +0800

    test

--- apps/home/apps.py
+++ apps/home/apps.py
@@ -2,12 +2,17 @@ from django.apps import AppConfig
 from .dht11_reader import start_reading_dht11
 from .gps_reader import start_reading_gps
 import io
+import logging
+
+log = logging.getLogger(__name__)
 
 def is_raspberrypi():
     try:
         with io.open('/sys/firmware/devicetree/base/model', 'r') as m:
-            if 'raspberry pi' in m.read().lower(): return True
-    except Exception: pass
+            if 'raspberry pi' in m.read().lower():
+                return True
+    except Exception as e:
+        log.error(f"Error checking Raspberry Pi: {e}")
     return False
 
 class SensorsConfig(AppConfig):
@@ -16,12 +21,14 @@ class SensorsConfig(AppConfig):
 
     def ready(self):
         if is_raspberrypi():
-            print("Starting DHT11 reader...")
-            start_reading_dht11()
-            print("Starting GPS reader...")
-            start_reading_gps()
+            try:
+                log.info("Starting DHT11 reader...")
+                start_reading_dht11()
+            except Exception as e:
+                log.error(f"Error starting DHT11 reader: {e}")
 
-
-
-
-
+            try:
+                log.info("Starting GPS reader...")
+                start_reading_gps()
+            except Exception as e:
+                log.error(f"Error starting GPS reader: {e}")
