d6c2fcb8f48979e31466423afc74c66d56750187
Lee Khoon Hong <69204505+KhoonHong@users.noreply.github.com>
2023-10-03 03:20:30 +0800

    lol

--- apps/home/motor.py
+++ apps/home/motor.py
@@ -1,34 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 import math
+import atexit
 
 # Pin definitions
-IN1 = 17
-IN2 = 18
+IN1, IN2, IN3, IN4, ENA, ENB = 17, 18, 22, 23, 21, 24
 
-IN3 = 22
-IN4 = 23
-
-ENA = 21
-ENB = 24
-
-GPIO.setwarnings(False)  # Suppress GPIO warnings
+# Suppress GPIO warnings
+GPIO.setwarnings(False)
 
 # Set up RPi.GPIO settings
-GPIO.setmode(GPIO.BCM)      # Use Broadcom pin numbering
-GPIO.setup(IN1, GPIO.OUT)   # Set IN1 as output
-GPIO.setup(IN2, GPIO.OUT)   # Set IN2 as output
-GPIO.setup(IN3, GPIO.OUT)   # Set IN2 as output
-GPIO.setup(IN4, GPIO.OUT)   # Set IN2 as output
-GPIO.setup(ENA, GPIO.OUT)
-GPIO.setup(ENB, GPIO.OUT)   # Set ENA as output
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup([IN1, IN2, IN3, IN4, ENA, ENB], GPIO.OUT)
 
 # Initialization
-pwmA = GPIO.PWM(ENA, 100)  # 100Hz frequency
-pwmB = GPIO.PWM(ENB, 100)
-
-pwmA.start(0)  # Initially start with 0% duty cycle = off
+pwmA, pwmB = GPIO.PWM(ENA, 100), GPIO.PWM(ENB, 100)
+pwmA.start(0)
 pwmB.start(0)
 
 def set_speed(speed_percentage):
@@ -36,34 +23,15 @@ def set_speed(speed_percentage):
     pwmB.ChangeDutyCycle(speed_percentage)
 
 def motor_forward():
-    #Left motor
-    GPIO.output(IN1, False)
-    GPIO.output(IN2, True)  
-    
-    #Right motor
-    GPIO.output(IN3, False)  
-    GPIO.output(IN4, True)  
-    
-    #Main motor
-    GPIO.output(ENA, True)
-    GPIO.output(ENB, True)
+    GPIO.output([IN1, IN3], False)
+    GPIO.output([IN2, IN4, ENA, ENB], True)
 
 def motor_backward():
-    
-    GPIO.output(IN1, True)   
-    GPIO.output(IN2, False)  
-    
-    GPIO.output(IN3, True)   
-    GPIO.output(IN4, False)  
-    
-    GPIO.output(ENA, True)
-    GPIO.output(ENB, True)
-
+    GPIO.output([IN1, IN3, ENA, ENB], True)
+    GPIO.output([IN2, IN4], False)
 
 def motor_stop():
-    GPIO.output(ENA, False)
-    GPIO.output(ENB, False)
-
+    GPIO.output([ENA, ENB], False)
 
 def set_speed_left(speed_percentage):
     pwmA.ChangeDutyCycle(speed_percentage)
@@ -72,46 +40,54 @@ def set_speed_right(speed_percentage):
     pwmB.ChangeDutyCycle(speed_percentage)
 
 def control_car(x, y):
-    if x == 0 and y == 0:  
+    NEAR_CENTER_THRESHOLD = 0.0005  # This threshold can be adjusted based on the sensitivity of the joystick.
+
+    # Check for near center values
+    if abs(x) < NEAR_CENTER_THRESHOLD and abs(y) < NEAR_CENTER_THRESHOLD:
         motor_stop()
         return
 
+    # Calculate angle and distance
     angle = math.degrees(math.atan2(y, x))
     if angle < 0:
         angle += 360
     distance = min(math.sqrt(x**2 + y**2), 1)
     speed = int(distance * 100)
-    
-    print(f"Angle: {angle}, Speed: {speed}")
-    print(f"Distance: {distance}, Speed: {speed}")
 
-    # Define the motor action for each direction range:
-    thresholds = [
-        (22.5, motor_stop),  # stop for slight nudges
-        (67.5, lambda: (motor_forward(), set_speed_left(int(speed * 0.5)), set_speed_right(speed))),  # Forward veering left
-        (112.5, lambda: (motor_forward(), set_speed_left(speed), set_speed_right(speed))),  # Forward straight
-        (157.5, lambda: (motor_forward(), set_speed_left(speed), set_speed_right(int(speed * 0.5)))),  # Forward veering right
-        (202.5, lambda: (motor_backward(), set_speed_left(speed), set_speed_right(int(speed * 0.5)))),  # Backward veering right
-        (247.5, lambda: (motor_backward(), set_speed_left(int(speed * 0.5)), set_speed_right(speed))),  # Backward veering left
-        (292.5, lambda: (motor_backward(), set_speed_left(speed), set_speed_right(speed))),  # Backward straight
-        (337.5, motor_stop),  # Stop near back point
-        (360, motor_stop)  # Stop at zero
-    ]
+    # Map the angle to motor behaviors:
 
+    # Forward veering left (0 to 90 degrees)
+    if 0 <= angle < 90:
+        motor_forward()
+        set_speed_left(speed * (1 - angle/90))
+        set_speed_right(speed)
+        return
 
-    for threshold, action in thresholds:
-        if angle < threshold:
-            action()
-            break
+    # Forward veering right (90 to 180 degrees)
+    if 90 <= angle < 180:
+        motor_forward()
+        set_speed_left(speed)
+        set_speed_right(speed * (1 - (angle - 90)/90))
+        return
 
+    # Backward veering right (180 to 270 degrees)
+    if 180 <= angle < 270:
+        motor_backward()
+        set_speed_left(speed * (angle - 180)/90)
+        set_speed_right(speed)
+        return
+
+    # Backward veering left (270 to 360 degrees)
+    if 270 <= angle <= 360:
+        motor_backward()
+        set_speed_left(speed)
+        set_speed_right(speed * (1 - (angle - 270)/90))
+        return
 
 
 def cleanup():
-    # Stops the motors
     motor_stop()
-    # Cleans up GPIO resources
     GPIO.cleanup()
 
-# It's a good practice to handle cleanup when the script ends.
-import atexit
+# Handle cleanup when the script ends
 atexit.register(cleanup)
