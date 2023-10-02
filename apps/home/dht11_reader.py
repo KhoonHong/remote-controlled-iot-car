3cd3afbf5ec801988a98f34af38e09a765dd7c3d
Lee Khoon Hong <69204505+KhoonHong@users.noreply.github.com>
2023-10-02 18:20:08 +0800

    test

--- apps/home/dht11_reader.py
+++ apps/home/dht11_reader.py
@@ -2,9 +2,9 @@ import time
 import threading
 import os
 import firebase_admin
+from firebase_admin import firestore
 
-
-# Check if we're on a development environment (e.g., Windows)
+# Check if we're in a development environment (e.g., Windows)
 IS_DEV = os.name == 'nt'  # 'nt' is the name for the Windows platform in Python
 
 if IS_DEV:
@@ -14,24 +14,26 @@ else:
     # Import the real module
     import Adafruit_DHT
 
-
 DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 26  # Assuming DHT11 is connected to GPIO4, change as needed
+DHT_PIN = 26  # Assuming DHT11 is connected to GPIO26, change as needed
 
 def store_to_firestore(temperature, humidity):
-    db = firebase_admin.firestore.client()
-    doc_ref = db.collection('dht11_data').document()
-    doc_ref.set({
-        'temperature': temperature,
-        'humidity': humidity,
-        'timestamp': firebase_admin.firestore.SERVER_TIMESTAMP
-    })
+    try:
+        db = firestore.client()
+        doc_ref = db.collection('dht11_data').document()
+        doc_ref.set({
+            'temperature': temperature,
+            'humidity': humidity,
+            'timestamp': firestore.SERVER_TIMESTAMP
+        })
+    except Exception as e:
+        print(f"Error saving to Firestore: {e}")
 
 def read_dht11():
     while True:
         humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
         if humidity is not None and temperature is not None:
-            print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+            print(f"Temp={temperature:0.1f}*C  Humidity={humidity:0.1f}%")
             store_to_firestore(temperature, humidity)
         else:
             print("Sensor failure. Check wiring.")
--- apps/home/gps_reader.py
+++ apps/home/gps_reader.py
@@ -2,36 +2,49 @@ import time
 import threading
 import serial
 import firebase_admin
+from firebase_admin import firestore
 
 # Setup UART communication parameters for GY-NEO6MV2 GPS module
 SERIAL_PORT = "/dev/serial0"  # Change if using a different UART port
 BAUD_RATE = 9600
 
 def store_to_firestore(latitude, longitude):
-    db = firebase_admin.firestore.client()
-    doc_ref = db.collection('gps_data').document()
-    doc_ref.set({
-        'latitude': latitude,
-        'longitude': longitude,
-        'timestamp': firebase_admin.firestore.SERVER_TIMESTAMP
-    })
+    try:
+        db = firestore.client()
+        doc_ref = db.collection('gps_data').document()
+        doc_ref.set({
+            'latitude': latitude,
+            'longitude': longitude,
+            'timestamp': firestore.SERVER_TIMESTAMP
+        })
+    except Exception as e:
+        print(f"Error saving to Firestore: {e}")
+
+def nmea_to_decimal(coord, direction):
+    # Converts NMEA format to decimal format
+    degrees = int(coord) // 100
+    minutes = coord - 100*degrees
+    decimals = degrees + minutes/60.0
+    if direction in ['S', 'W']:
+        decimals = -decimals
+    return decimals
 
 def read_gps():
     with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
         while True:
-            line = ser.readline().decode('utf-8')
-            if line.startswith('$GPGLL'):
-                parts = line.split(',')
-                latitude = float(parts[1])  # Convert to desired format as needed
-                longitude = float(parts[3])  # Convert to desired format as needed
-                print("Latitude: %s \t Longitude: %s" % (latitude, longitude))
-                store_to_firestore(latitude, longitude)
+            try:
+                line = ser.readline().decode('utf-8').strip()
+                if line.startswith('$GPGLL'):
+                    parts = line.split(',')
+                    latitude = nmea_to_decimal(float(parts[1]), parts[2])
+                    longitude = nmea_to_decimal(float(parts[3]), parts[4])
+                    print(f"Latitude: {latitude} \t Longitude: {longitude}")
+                    store_to_firestore(latitude, longitude)
+            except Exception as e:
+                print(f"Error reading GPS data: {e}")
             time.sleep(10)
 
 def start_reading_gps():
     thread = threading.Thread(target=read_gps)
     thread.daemon = True
     thread.start()
-
-
-
